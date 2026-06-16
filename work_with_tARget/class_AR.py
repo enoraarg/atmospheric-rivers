b11cb06633194bcc0f21e2d9a2e921373e5d4602
Enora Le Gall <elegall@espri-calcul-01.ipsl.fr>
2026-06-16 13:39:50 +0200

    correction de l'erreur source

--- work_with_tARget/class_AR.py
+++ work_with_tARget/class_AR.py
@@ -42,9 +42,9 @@ path.append('/home/elegall/AR/scripts')
 from IBTrACS.get_storms import which_storm_associated_to_mask
 #import config as cf
 
-import source.local_paths as local
-import source.manage_maps as maps
-import source.open_files as open_local
+import auxi_src.local_paths as local
+import auxi_src.manage_maps as maps
+import auxi_src.open_files as open_local
 
 # ----------------------------------------------
 #%%             Auxilliary
